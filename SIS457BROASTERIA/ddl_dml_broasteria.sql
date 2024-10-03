8eadc22fb2eabfc002780841ff22d44f8a033966
rodrigo carreon lambertin <carreonlambertinrodrigo@gmail.com>
2024-10-03 12:37:34 -0400

    creacion de la base de datos

---
+++ SIS457BROASTERIA/ddl_dml_broasteria.sql
@@ -0,0 +1,123 @@
+﻿CREATE DATABASE LabBroasteria
+GO
+USE [master]
+GO
+CREATE LOGIN [usrbroasteria] WITH PASSWORD = N'123456',
+	DEFAULT_DATABASE = [LabBroasteria],
+	CHECK_EXPIRATION = OFF,
+	CHECK_POLICY = ON
+GO
+USE [LabBroasteria]
+GO
+CREATE USER [usrbroasteria] FOR LOGIN [usrbroasteria]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [usrbroasteria]
+GO
+
+CREATE TABLE Proveedor (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  nit BIGINT NOT NULL,
+  nombre varchar(100) NOT NULL,
+  razonSocial VARCHAR(100) NOT NULL,
+  direccion VARCHAR(250) NULL,
+  telefono VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Compra (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idProveedor INT NOT NULL,
+  transaccion INT NOT NULL,
+  total DECIMAL NOT NULL,
+  fecha DATE NOT NULL DEFAULT GETDATE(),
+  CONSTRAINT fk_Compra_Proveedor FOREIGN KEY (idProveedor) REFERENCES Proveedor(id)
+);
+
+CREATE TABLE Producto (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  codigo VARCHAR(20) NOT NULL,
+  descripcion VARCHAR(250) NOT NULL,
+  stock INT NOT NULL DEFAULT 0,
+  precioVenta DECIMAL NOT NULL CHECK (precioVenta > 0)
+);
+
+CREATE TABLE CompraDetalle (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idCompra INT NOT NULL,
+  idProducto INT NOT NULL,
+  cantidad DECIMAL NOT NULL CHECK (cantidad > 0),
+  precioUnitario DECIMAL NOT NULL,
+  total DECIMAL NOT NULL,
+  CONSTRAINT fk_CompreDetalle FOREIGN KEY (idCompra) REFERENCES Compra(id),
+  CONSTRAINT fk_CompraDetalle FOREIGN KEY (idProducto) REFERENCES Producto(id)
+);
+
+
+
+CREATE TABLE Empleado (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  cedulaIdentidad VARCHAR(12) NOT NULL,
+  nombres VARCHAR(30) NOT NULL,
+  primerApellido VARCHAR(30) NULL,
+  segundoApellido VARCHAR(30) NULL,
+  direccion VARCHAR(250) NOT NULL,
+  celular BIGINT NOT NULL,
+  cargo VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Usuario (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idEmpleado INT NOT NULL,
+  usuario VARCHAR(15) NOT NULL,
+  clave VARCHAR(250) NOT NULL,
+  CONSTRAINT fk_Usuario_Empleado FOREIGN KEY (idEmpleado) REFERENCES Empleado(id)
+);
+
+CREATE TABLE Venta (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idUsuario INT NOT NULL,
+  transaccion INT NOT NULL,
+  fecha DATE NOT NULL DEFAULT GETDATE(),
+  CONSTRAINT fk_Venta_Usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
+);
+
+CREATE TABLE VentaDetalle (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idVenta INT NOT NULL,
+  idProducto INT NOT NULL,
+  cantidad DECIMAL NOT NULL CHECK (cantidad > 0),
+  precioUnitario DECIMAL NOT NULL,
+  total DECIMAL NOT NULL,
+  CONSTRAINT fk_VentaDetalle_Venta FOREIGN KEY (idVenta) REFERENCES Venta(id)
+);
+
+CREATE TABLE Cliente (
+    id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    cedulaIdentidad VARCHAR(12) NOT NULL,
+	nombres VARCHAR(30) NOT NULL,
+	primerApellido VARCHAR(30) NULL,
+	segundoApellido VARCHAR(30) NULL,
+	direccion VARCHAR(250) NOT NULL,
+	celular BIGINT NOT NULL
+);
+
+ALTER TABLE Producto ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Producto ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Producto ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+GO
+ALTER PROC paProductoListar @parametro VARCHAR(100)
+AS
+  SELECT * FROM Producto
+  WHERE estado<>-1 AND descripcion LIKE '%'+REPLACE(@parametro, ' ', '%')+'%'
+  ORDER BY descripcion;
+
+INSERT INTO Producto(codigo, descripcion, stock, precioVenta)
+VALUES ('PO1010', 'Pollo 1/4', 1, 50);
+
+INSERT INTO Producto(codigo, descripcion, stock, precioVenta)
+VALUES ('SO2020', 'Soda Coca cola', 30, 10);
+
+INSERT INTO Producto(codigo, descripcion, stock, precioVenta)
+VALUES ('HA', 'Hamburguesa', 12, 8);
+
+SELECT * FROM Producto
