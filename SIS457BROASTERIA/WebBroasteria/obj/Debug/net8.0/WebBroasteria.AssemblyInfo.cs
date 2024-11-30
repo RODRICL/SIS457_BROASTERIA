e07d0be0a9caea26d518f5271cc9a1d8b2869a52
rodrigo carreon lambertin <carreonlambertinrodrigo@gmail.com>
2024-11-30 10:07:19 -0400

    proyecto final

--- SIS457BROASTERIA/WebBroasteria/obj/Debug/net8.0/WebBroasteria.AssemblyInfo.cs
+++ SIS457BROASTERIA/WebBroasteria/obj/Debug/net8.0/WebBroasteria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBroasteria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb045d53a1b250a4f23f4fdbb6c7dbd18f0f6c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c5c310ace59f3aa7a6339a67027675e0a9fcd7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBroasteria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBroasteria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
